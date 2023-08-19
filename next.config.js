828962106dbdade5a0fa1f17a75604c3794c8eca
Martynas-J <Aurimhas@gmail.com>
2023-08-19 09:36:46 +0300

    add facebook login v1.1

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
         API_URL1: "https://next-baigiamasis.vercel.app/api",
         GOOGLE_ID: "444899498652-lt45t8iafifcm93perrsilj1a44gin08.apps.googleusercontent.com",
         GOOGLE_SECRET: "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",
-        FACEBOOK_CLIENT_ID: "1393785324537739",
-        FACEBOOK_CLIENT_SECRET: "fff04f57364fb1c8cd8e867237f9c3f1",
+        FACEBOOK_CLIENT_ID: "621483989824015",
+        FACEBOOK_CLIENT_SECRET: "ef91275fc5b9c5c81bdb4170f9d061fb",
 
 
         NEXTAUTH_SECRET: "qweewqasddsa",
